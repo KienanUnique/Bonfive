bbf2a182fd913cadd69bf53432f11a4f16595b23
KienanUnique <69349336+KienanUnique@users.noreply.github.com>
2023-01-22 14:27:51 +0500

    Refacor EnemyAttackTriggerZonePlayerDetector

--- Assets/Script/Enemies/EnemyAttackTriggerZonePlayerDetector.cs
+++ Assets/Script/Enemies/EnemyAttackTriggerZonePlayerDetector.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 [RequireComponent(typeof(CircleCollider2D))]
 public class EnemyAttackTriggerZonePlayerDetector : MonoBehaviour
 {
-    public bool IsPlayerInsideTriggerZone => _triggerZone.bounds.Contains(GameController.Instance.Player.CurrentPosition);
+    public bool IsPlayerInsideTriggerZone {get; private set;}
     private CircleCollider2D _triggerZone;
     private void Awake()
     {
         _triggerZone = GetComponent<CircleCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.TryGetComponent(out PlayerController playerController))
+        {
+            IsPlayerInsideTriggerZone = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.TryGetComponent(out PlayerController playerController))
+        {
+            IsPlayerInsideTriggerZone = false;
+        }
     }
 }
