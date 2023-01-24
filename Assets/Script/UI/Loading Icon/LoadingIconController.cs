7c2a23c4fc8d22e81566f1f819744b77e250a8a9
KienanUnique <69349336+KienanUnique@users.noreply.github.com>
2023-01-24 07:37:36 +0500

    Add loading animation

---
+++ Assets/Script/UI/Loading Icon/LoadingIconController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class LoadingIconController : MonoBehaviour
+{
+    [SerializeField] private GameObject _loadingIconImageGameObject;
+    [SerializeField] private float _rotationSpeed;
+    private bool _animationStarted;
+
+    public void StartLoadingAnimation()
+    {
+        if (_animationStarted)
+        {
+            return;
+        }
+        _loadingIconImageGameObject.SetActive(true);
+        _animationStarted = true;
+    }
+    private void Start()
+    {
+        _loadingIconImageGameObject.SetActive(false);
+        _animationStarted = false;
+    }
+
+    private void Update()
+    {
+        if (_animationStarted)
+        {
+            _loadingIconImageGameObject.transform.Rotate(0, 0, -1 * Time.deltaTime * _rotationSpeed, Space.Self);
+        }
+    }
+}
